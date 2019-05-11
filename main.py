915dd1fe6084840abf6eed176f96c4485cc06430
Rick Riehle <rickr@bitbamboo.com>
2019-05-11 00:36:00 -0700

    process json via stdio

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import json
+from sys import stdin
+
+from hand import Hand
+
 
 def main():
-    pass
+    """
+    Take a series of cards as json from stdin,
+    load them into a hand and print them out,
+    merely to demonstrate that the class/program works.
+
+    usage:
+
+    $ python main.py < hand.json
+    """
+
+    for line in stdin:
+        print(json.dumps(str(Hand(json.loads(line)))))
 
 
 if __name__ == '__main__':
